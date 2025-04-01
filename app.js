f6ea8b8144472548e6d97a322f8a76bd162d0a68
Tharun Guda <tharunguda7@gmail.com>
2025-04-01 14:49:50 -0500

    new changes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cakesRouter = require('./routes/cakes');
 
+var gridRouter = require('./routes/grid');
 
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cakes', cakesRouter);
+app.use('/grid', gridRouter);
 
 
 
