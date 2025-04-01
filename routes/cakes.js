66402c2375de5ae452c698f2e2f5309d99277d71
Tharun Guda <tharunguda7@gmail.com>
2025-04-01 14:27:05 -0500

    changes

--- routes/cakes.js
+++ routes/cakes.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const app = express();
+//- cakes.js
+var express = require('express');
+var app = express();
 
-// Define your route for cakes
-app.get('/cakes', (req, res) => {
-    // Declare the results array on the server-side
-    const results = [
-        { name: "Chocolate Cake", flavor: "Chocolate", price: 20 },
-        { name: "Red Velvet", flavor: "Red Velvet", price: 25 },
-        { name: "Strawberry Cake", flavor: "Strawberry", price: 18 }
-    ];
+// Serve static files
+app.use(express.static('public'));
 
-    // Pass the results array to the Pug view
-    res.render('cakes', { results: results });
+// Set view engine
+app.set('view engine', 'pug');
+
+// Endpoint for Cakes
+app.get('/cakes', function(req, res) {
+    res.render('cakes'); // This will render cakes.pug
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Start the server
+var server = app.listen(3000, function() {
+    console.log('Server running on http://localhost:3000');
 });
