66419544afa2cde97e1912be47cd62ff017c2fbd
Tharun Guda <tharunguda7@gmail.com>
2025-04-01 02:16:29 -0500

    changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cakes', cakessRouter);
+app.use('/cakes', cakesRouter);
 
 
 // catch 404 and forward to error handler
