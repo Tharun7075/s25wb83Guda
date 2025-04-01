e6e38a50789744e5e5017ba2f35709fed32410aa
Tharun Guda <tharunguda7@gmail.com>
2025-04-01 13:33:21 -0500

    new changes

--- routes/cakes.js
+++ routes/cakes.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET cakes page. */
-router.get('/', function(req, res, next) {
-  const results = [
+router.get('/', function (req, res, next) {
+  var results = [
     { flavor: "Chocolate", size: "Large", price: 15 },
     { flavor: "Vanilla", size: "Medium", price: 10 },
     { flavor: "Strawberry", size: "Small", price: 8 }
   ];
 
-  res.render('cakes', { results });
+  res.render('cakes', { results: results }); // Pass results to Pug template
 });
 
 module.exports = router;
