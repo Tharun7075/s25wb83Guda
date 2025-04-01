84e7bd38565754bda3d309f4433e9372c092e6cc
Tharun Guda <tharunguda7@gmail.com>
2025-04-01 15:44:00 -0500

    new changes

--- routes/grid.js
+++ routes/grid.js
@@ -4,8 +4,6 @@ var router = express.Router();
 /* GET grid page with query parameters for rows and columns. */
 router.get('/', function(req, res, next) {
   const{rows, cols }=req.query;
-  
-  
   res.render('grid', { title: 'Grid Display', query:{rows, cols}});
 });
 
