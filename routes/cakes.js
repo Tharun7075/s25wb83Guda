f02b108f2d9bc34b42eca41857915fca278290e6
Tharun Guda <tharunguda7@gmail.com>
2025-04-01 14:39:07 -0500

    new changes

--- routes/cakes.js
+++ routes/cakes.js
@@ -4,11 +4,13 @@ var router = express.Router();
 
 /* GET cakes search results. */
 router.get('/', function(req, res, next) {
+  // Define the array of cakes in the route handler
   var results = [
     {name: 'Chocolate Cake', flavors: 'Chocolate, Vanilla', price: 15.99},
     {name: 'Red Velvet Cake', flavors: 'Red Velvet, Cream Cheese', price: 20.99},
     {name: 'Carrot Cake', flavors: 'Carrot, Walnut', price: 18.50}
   ];
+  // Render the 'cakes' template and pass the results array to it
   res.render('cakes', { results: results });
 });
 
